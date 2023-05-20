f8cd0926a98ece5fc9985014216d573ee6592fbf
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-05-20 16:47:06 +0200

    chore: Moved bill processing logic from screen to function

--- src/app/Screens/ScanScreen.tsx
+++ src/app/Screens/ScanScreen.tsx
@@ -4,13 +4,13 @@ import { Camera, CameraType } from 'expo-camera'
 import { Container, ContentContainer, FlexEndContainer, CenteredContainer } from '../Components/Design/Container'
 import { Button } from '../Components/Design/Button'
 import { Camera as StCamera } from '../Components/Shared/Camera'
-import { processBill } from '../../core/ocr/ProcessBillAsync'
-import { Bill, Entity, Item, ResponseData } from '../types'
+import { processBillAsync } from '../../core/ocr/ProcessBillAsync'
+import { Bill } from '../types'
 
 const ScanScreen = () => {
   const [permission, requestPermission] = Camera.useCameraPermissions()
   const cameraRef = useRef<Camera>(null)
-  const [data, setData] = useState<ResponseData>()
+  const [data, setData] = useState<Bill | undefined>() // TODO: remove this after testing
   
   if (!permission) {
     // Camera permissions are still loading
@@ -44,7 +44,7 @@ const ScanScreen = () => {
   const scanBill = async (pictureBase64: string) => {
     if (pictureBase64) {
       try {
-        return processBill(pictureBase64)
+        return processBillAsync(pictureBase64)
       } catch {
         console.log('No ocr')
         return
@@ -59,86 +59,9 @@ const ScanScreen = () => {
   const onScan = async () => {
     picture = await takePicture()
     if (picture && picture.base64) {
-      const data = await scanBill(picture.base64) // TODO: always enable this after testing
-      if (data) {
-        if (data.document && data.document.entities) {
-          setData(data) //TODO: always enable this after testing
-          const bill: Bill = {
-            items: [],
-            total: 0,
-            currency: ''
-          }
-          data.document.entities.forEach((entity: Entity) => {
-            switch (entity.type){
-              case 'line_item':
-                const item: Item = {
-                  name:'',
-                  quantity: 0,
-                  totalPrice: 0,
-                  notes: [],
-                }
-                entity.properties.forEach((property) => {
-                  switch (property.type) {
-                    case 'line_item/amount':
-                      const totalPrice = parseFloat(property.mentionText)
-                      typeof totalPrice === 'number' && !isNaN(totalPrice) ? item.totalPrice = totalPrice : item.notes!.push('Could not establish the total price for this item.')
-                      break
-                    case 'line_item/description':
-                      item.description = property.mentionText
-                      break
-                    case 'line_item/quantity':
-                      while (isNaN(parseInt(property.mentionText[0]))) {
-                        property.mentionText = property.mentionText.slice(1)
-                      }
-                      while (isNaN(parseInt(property.mentionText[property.mentionText.length - 1]))) {
-                        property.mentionText = property.mentionText.slice(0, -1)
-                      }
-                      const quantity = parseInt(property.mentionText)
-                      typeof quantity === 'number' && !isNaN(quantity) ? item.quantity = quantity : item.notes!.push('Could not establish the quantity of this item.')
-                      break
-                    case 'line_item/unit':
-                      item.name = property.mentionText
-                      break
-                    case 'line_item/unit_price':
-                      const price = parseFloat(property.mentionText)
-                      typeof price === 'number' && !isNaN(price) ? item.price = price : item.notes!.push('Could not establish the unit price for this item.')
-                      break
-                  }
-                })
-                if (!item.price) {
-                  item.totalPrice && item.quantity ? item.price = item.totalPrice / item.quantity : item.notes!.push('Could not establish the unit price for this item.')
-                }
-                if (!item.quantity) {
-                  item.totalPrice && item.price ? item.quantity = item.totalPrice / item.price : item.notes!.push('Could not establish the quantity of this item.')
-                }
-                if (!item.totalPrice) {
-                  item.quantity && item.price ? item.totalPrice = item.quantity * item.price : item.notes!.push('Could not establish the total price for this item.')
-                }
-                if (!item.name) {
-                  if (item.description) {
-                    item.name = item.description 
-                    item.description = undefined
-                  } else {
-                    item.notes!.push('Could not establish the name of this item.')
-                  }
-                }
-                bill.items.push(item)
-                break
-              case 'net_amount':
-                bill.total = parseFloat(entity.mentionText)
-                break
-              case 'currency':
-                bill.currency = entity.mentionText
-                break
-            } 
-          })
-          console.log(bill) // TODO: delete this after testing
-          return
-        }
-        console.log('No entities')
-      } else {
-        console.log('No data')
-      }
+      const bill = await scanBill(picture.base64) // TODO: always enable this after testing
+      setData(bill) // TODO: always enable this after testing
+      console.log(bill)
       cameraRef.current?.resumePreview()
       return
     }
--- src/core/ocr/ProcessBillAsync.ts
+++ src/core/ocr/ProcessBillAsync.ts
@@ -1,9 +1,9 @@
 import Constants from 'expo-constants'
-import { ResponseData } from '../../app/types'
+import { Bill, Entity, Item } from '../../app/types'
 
 const { LOCATION, PROJECT_ID, PROCESSOR_ID, PROCESSOR_AUTH_TOKEN } = Constants.manifest.extra
 
-export const processBill = async (pictureBase64: string): Promise<ResponseData> => {
+export const processBillAsync = async (pictureBase64: string): Promise<Bill | undefined> => {
   const response = await fetch(`https://${LOCATION}-documentai.googleapis.com/v1/projects/${PROJECT_ID}/locations/${LOCATION}/processors/${PROCESSOR_ID}:process`,
     {
       method: 'POST',
@@ -22,5 +22,85 @@ export const processBill = async (pictureBase64: string): Promise<ResponseData> 
     }
   )
   const json = await response.json()
-  return json
+  if (json) {
+    if (json.document && json.document.entities) {
+      const bill: Bill = {
+        items: [],
+        total: 0,
+        currency: ''
+      }
+      json.document.entities.forEach((entity: Entity) => {
+        switch (entity.type){
+          case 'line_item':
+            const item: Item = {
+              name:'',
+              quantity: 0,
+              totalPrice: 0,
+              notes: [],
+            }
+            entity.properties.forEach((property) => {
+              switch (property.type) {
+                case 'line_item/amount':
+                  const totalPrice = parseFloat(property.mentionText)
+                  typeof totalPrice === 'number' && !isNaN(totalPrice) ? item.totalPrice = totalPrice : item.notes!.push('Could not establish the total price for this item.')
+                  break
+                case 'line_item/description':
+                  item.description = property.mentionText
+                  break
+                case 'line_item/quantity':
+                  let quantity = property.mentionText
+                  if (isNaN(parseInt(quantity)) && quantity.length > 3 && !item.name) {
+                    item.name = quantity.split(' ')[1]
+                  }
+                  while (isNaN(parseInt(quantity[0]))) {
+                    quantity = quantity.slice(1)
+                  }
+                  while (isNaN(parseInt(quantity[quantity.length - 1]))) {
+                    quantity = quantity.slice(0, -1)
+                  }
+                  typeof quantity === 'number' && !isNaN(quantity) ? item.quantity = quantity : item.notes!.push('Could not establish the quantity of this item.')
+                  break
+                case 'line_item/unit':
+                  item.name = property.mentionText
+                  break
+                case 'line_item/unit_price':
+                  const price = parseFloat(property.mentionText)
+                  typeof price === 'number' && !isNaN(price) ? item.price = price : item.notes!.push('Could not establish the unit price for this item.')
+                  break
+              }
+            })
+            if (!item.price) {
+              item.totalPrice && item.quantity ? item.price = item.totalPrice / item.quantity : item.notes!.push('Could not establish the unit price for this item.')
+            }
+            if (!item.quantity) {
+              item.totalPrice && item.price ? item.quantity = item.totalPrice / item.price : item.notes!.push('Could not establish the quantity of this item.')
+            }
+            if (!item.totalPrice) {
+              item.quantity && item.price ? item.totalPrice = item.quantity * item.price : item.notes!.push('Could not establish the total price for this item.')
+            }
+            if (!item.name) {
+              if (item.description) {
+                item.name = item.description 
+                item.description = undefined
+              } else {
+                item.notes!.push('Could not establish the name of this item.')
+              }
+            }
+            bill.items.push(item)
+            break
+          case 'net_amount':
+            bill.total = parseFloat(entity.mentionText)
+            break
+          case 'currency':
+            bill.currency = entity.mentionText
+            break
+        } 
+      })
+      return bill
+    }
+    console.log('No entities')
+  } else {
+    console.log('No data')
+  }
+  return
 }

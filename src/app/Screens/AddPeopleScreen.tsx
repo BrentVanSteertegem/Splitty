0da84a786c02a90ae24cea79297524aac20bf0d0
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-05-23 18:25:22 +0200

    feat: Finished AddPeopleScreen

--- src/app/Screens/AddPeopleScreen.tsx
+++ src/app/Screens/AddPeopleScreen.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Navigation } from '../../core/navigation'
-import { Button, CenteredContainer, ContentContainer, LargeVerticalPadding, FlexEndContainer, Text, TextInput, SmallVerticalPadding, MediumVerticalPadding } from '../Components'
+import { Button, CenteredContainer, ContentContainer, LargeVerticalPadding, FlexEndContainer, Text, TextInput, SmallVerticalPadding, MediumVerticalPadding, Offset, PeopleSetter } from '../Components'
+import { PersonProps } from '../types'
 
 const AddPeopleScreen = ({ navigation, route }) => {
   const { bill } = route.params
@@ -12,25 +13,43 @@ const AddPeopleScreen = ({ navigation, route }) => {
   }, [])
 
   const [billName, setBillName] = useState<string>('')
+  const [people, setPeople] = useState<PersonProps[]>([{
+    name: 'You',
+    items: [],
+    total: 0
+  }])
 
   return bill ? (
     <ContentContainer>
       <LargeVerticalPadding />
-      <Text fontSize='large' offset={true}>Bill name</Text>
+      <Offset>
+        <Text fontSize='large'>Bill name</Text>
+      </Offset>
       <TextInput 
         onChangeText={setBillName}
         placeholder='Restaurant name e.g.'
       />
       <MediumVerticalPadding />
-      <Text fontSize='large' offset={true}>People</Text>
-      <Text offset={true}>You</Text>
-      <Text offset={true}>Chantal</Text>
-      <Text offset={true}>Joske</Text>
-      <Button faIconLeft='plus'>Add person</Button>
+      <Offset>
+        <Text fontSize='large'>People</Text>
+      </Offset>
+      <PeopleSetter
+        people={people}
+        setPeople={setPeople}
+      />
       <CenteredContainer>
         <FlexEndContainer>
           <Button
-            onPress={() => window.alert(billName)}
+            onPress={() => navigation.navigate(
+              Navigation.SCANNAVIGATOR, { 
+                screen: Navigation.ADDITEMS, 
+                params: { 
+                  billName,
+                  people,
+                  bill
+                } 
+              }
+            )}
           >
             Split expenses
           </Button>

77de9a76820f7837f0e01b34058e8e7809c743ce
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-06-10 14:39:54 +0200

    chore: Added ManagePeople component to AddPeopleScreen

--- src/app/Screens/AddPeopleScreen.tsx
+++ src/app/Screens/AddPeopleScreen.tsx
@@ -1,11 +1,9 @@
-import { useEffect, useState, useRef } from 'react'
-import { ScrollView } from 'react-native'
+import { useEffect } from 'react'
 import { Navigation } from '../../core/navigation'
-import { Button, CenteredContainer, ContentContainer, LargeVerticalPadding, Text, TextInput, SmallVerticalPadding, MediumVerticalPadding, PeopleSetter, FullScreenContainer, Container } from '../Components'
-import { Person } from '../types'
-import { MediumHorizontalPadding } from '../Components'
+import { NavigationProps } from '../types'
+import { ManagePeople } from '../Components'
 
-const AddPeopleScreen = ({ navigation, route }) => {
+const AddPeopleScreen = ({ navigation, route }: NavigationProps) => {
   const { bill } = route.params
   
   useEffect(() => {
@@ -14,65 +12,13 @@ const AddPeopleScreen = ({ navigation, route }) => {
     }
   }, [])
   
-  const [people, setPeople] = useState<Person[]>(bill.people)
-  
-  const addPerson = (newPeople: Person[]) => {
-    bill.people = newPeople
-    setPeople(newPeople)
-  }
-
-  const setBillName = (text: string) => {
-    bill.name = text
-  }
-
-  const svRef = useRef<ScrollView>(null)
-  
   return bill ? (
-    <ScrollView
-      ref={svRef}
-      onContentSizeChange={() => svRef.current!.scrollToEnd({ animated: true })}
-      >
-      <FullScreenContainer>
-        <ContentContainer>
-          <LargeVerticalPadding />
-          <MediumHorizontalPadding>
-            <Text fontSize='large'>Bill name</Text>
-          </MediumHorizontalPadding>
-          <TextInput 
-            onChangeText={setBillName}
-            placeholder='Restaurant name e.g.'
-            />
-          <MediumVerticalPadding />
-          <MediumHorizontalPadding>
-            <Text fontSize='large'>People</Text>
-            <PeopleSetter
-              people={people}
-              setPeople={addPerson}
-            />
-          </MediumHorizontalPadding>
-          <MediumVerticalPadding />
-          <Container
-            justifyContent='flex-end'
-          >
-            <Button
-              fontSize='large'
-              size='full-width'
-              onPress={() => navigation.navigate(
-                Navigation.SCANNAVIGATOR, { 
-                  screen: Navigation.ADDITEMS, 
-                  params: {
-                    people,
-                    bill
-                  } 
-                }
-              )}
-            >
-              Split bill
-            </Button>
-          </Container>
-        </ContentContainer>
-      </FullScreenContainer>
-    </ScrollView>
+    <ManagePeople
+      bill={bill}
+      navigation={navigation}
+      buttonNavigator={Navigation.SCANNAVIGATOR}
+      nextButtonScreen={Navigation.ADDITEMS}
+    />
   ) : null
 }
 

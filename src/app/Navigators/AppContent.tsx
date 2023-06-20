dbc7cf02eb04efa242a1f04c05bab60bc79bd09e
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2023-06-20 10:10:54 +0200

    feat: Check if app is launched for the first time before showing AuthScreen

--- src/app/Navigators/AppContent.tsx
+++ src/app/Navigators/AppContent.tsx
@@ -1,21 +1,40 @@
+import { useEffect, useState } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Navigation } from '../../core/navigation'
 import AppNavigator from './AppNavigator'
 import AuthNavigator from './AuthNavigator'
 import { useAuthContext } from '../Components/Shared/Auth/AuthProvider'
+import { getData, storeData } from '../../core/storage/StoreData'
 
 const AppContent = () => {
   const { isLoggedIn } = useAuthContext()
+  const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | undefined>(undefined)
 
   const Stack = createNativeStackNavigator()
 
-  return (
+  useEffect(() => {
+    const handleFirstLaunch =  async () => {
+      setIsFirstLaunch(true)
+      await storeData('isNotFirstLaunch', true)
+    }
+
+    const checkIfFirstLaunch = async () => {
+      const isNotFirstLaunch = await getData('isNotFirstLaunch')
+      isNotFirstLaunch ? 
+      setIsFirstLaunch(false) :
+      handleFirstLaunch()
+    }
+
+    checkIfFirstLaunch()
+  }, [])
+
+  return isLoggedIn !== undefined && isFirstLaunch !== undefined ? (
     <Stack.Navigator
       screenOptions={{
         headerShown: false
       }}
     >
-      {!isLoggedIn && 
+      {!isLoggedIn && isFirstLaunch &&
         <Stack.Screen
           name={Navigation.AUTHNAVIGATOR}
           component={AuthNavigator}
@@ -26,7 +45,7 @@ const AppContent = () => {
         component={AppNavigator}
       />
     </Stack.Navigator>
-  )
+  ) : null
 }
 
 export default AppContent
